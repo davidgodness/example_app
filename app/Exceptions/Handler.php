1cfc475e0e2807be4409d88d9d4e5e3f09e8d394
davidchang <daaniloveyou@gmail.com>
2024-04-23 19:49:40 +0800

    add error_log logic

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Http\ApiResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Throwable;
 
@@ -32,16 +33,44 @@ class Handler extends ExceptionHandler
         });
 
         $this->renderable(function (Throwable $e) {
-            if (config('app.debug')) {
+            $code = $e->getCode();
+            $message = $e->getMessage();
+            $exception = get_class($e);
+            $file = $e->getFile();
+            $line = $e->getLine();
+            $trace = collect($e->getTrace())->map(fn($trace) => Arr::except($trace, ['args']));
 
-                $data = ApiResponse::error($e->getCode(), $e->getMessage(), [], [
-                    'exception' => get_class($e),
-                    'file' => $e->getFile(),
-                    'line' => $e->getLine(),
-                    'trace' => collect($e->getTrace())->map(fn($trace) => Arr::except($trace, ['args']))->all(),
-                ]);
+            $tableName = 'error_logs_' . now()->toDateTime()->format('Y_m_d');
+
+            $results = DB::select("show tables like '$tableName'");
+
+            if (empty($results)) {
+                DB::statement("CREATE TABLE `$tableName` (
+                    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    `code` INT(10) NOT NULL DEFAULT 0,
+                    `message` VARCHAR(255) NOT NULL DEFAULT '',
+                    `exception` VARCHAR(255),
+                    `file` VARCHAR(255),
+                    `line` INT(11),
+                    `trace` LONGTEXT,
+                    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                )");
+            }
+
+            DB::table($tableName)->insert([
+                'code' => $code,
+                'message' => $message,
+                'exception' => $exception,
+                'file' => $file,
+                'line' => $line,
+                'trace' => $trace->toJson(),
+            ]);
+
+            if (config('app.debug')) {
+                $data = ApiResponse::error($code, $message, [], compact('exception', 'file', 'line', 'trace'));
             } else {
-                $data = ApiResponse::error($e->getCode(), $e->getMessage());
+                $data = ApiResponse::error($code, $message);
             }
 
             return new JsonResponse(
